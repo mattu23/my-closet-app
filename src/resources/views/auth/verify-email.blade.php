9bba27f253c69ab593aecce4fcab6077affbc596
mattu23 <hirokiii_27@yahoo.co.jp>
2025-05-05 20:25:45 +0900

    Laravel Breezeの導入

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('メールアドレスの確認が必要です。') }}
+        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('新しい確認リンクがメールアドレスに送信されました。') }}
+            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
 
@@ -15,7 +15,7 @@
 
             <div>
                 <x-primary-button>
-                    {{ __('確認メールを再送信') }}
+                    {{ __('Resend Verification Email') }}
                 </x-primary-button>
             </div>
         </form>
@@ -24,8 +24,8 @@
             @csrf
 
             <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('ログアウト') }}
+                {{ __('Log Out') }}
             </button>
         </form>
     </div>
-</x-guest-layout> 
+</x-guest-layout>
